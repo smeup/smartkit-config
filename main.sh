961ad57b954e1a302a390ce3a444a5ae17a7669f
Franco Parodi <franco.parodi@smeup.com>
2019-09-20 11:24:42 +0200

    Aggiornamento degli script

--- main.sh
+++ main.sh
@@ -2,20 +2,20 @@
 
 while true
 do
-menuChoose=$(whiptail --title "Menu configurazione" --menu "Configurazione:" 25 78 16 \
-"Base" "Configurazioni di base." \
-"Paths" "Percorsi Windows/Linux" 3>&1 1>&2 2>&3)
 
-exitstatus=$?
-if [ "$exitstatus" = 0 ]; then
-    if [ "$menuChoose" = "Base" ]; then
-        source smartkitconf.sh
-    elif [ "$menuChoose" = "Paths" ]; then
-        source pathsConf.sh
+    menuChoose=$(whiptail --title "Menu configurazione" --menu "Configurazione:" 25 78 16 \
+    "Base" "Configurazioni di base." \
+    "Paths" "Percorsi Windows/Linux" 3>&1 1>&2 2>&3)
+
+    exitstatus=$?
+    if [ "$exitstatus" = 0 ]; then
+        if [ "$menuChoose" = "Base" ]; then
+            source smartkitconf.sh
+        elif [ "$menuChoose" = "Paths" ]; then
+            source pathsConf.sh
+        fi
     else
         break
     fi
-else
-    break
-fi
+
 done
--- pathsConf.sh
+++ pathsConf.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 function paths_configuration () {
 
+    # TODO 
+    # Caricare percorsi esistenti da file di configurazione
+    _WIN_LOADED[0]="Percorso Windows 1"
+    _LIN_LOADED[0]="Percorso Linux 1"
+    _WIN_LOADED[1]="Percorso Windows 2"
+    _LIN_LOADED[1]="Percorso Linux 2"
+    _WIN_LOADED[2]="Percorso Windows 3"
+    _LIN_LOADED[2]="Percorso Linux 3"
+
     # Crea variabili _WINnn _LINnn
     for ((n=0;n<$_PATH_NUM;n++))
     do
@@ -8,18 +17,16 @@ function paths_configuration () {
         _LIN[$n]=""
         
         # Remapping windows/linux path WINnn-LINnn
-        _WIN[$n]=$(whiptail --title "Paths configuration ($n)" --backtitle "Paths configuration ($n)" --inputbox "Windows:" $_BOX_H $_BOX_W "" 3>&1 1>&2 2>&3)
+        _WIN[$n]=$(whiptail --title "${_TITLE} ($n di $_PATH_NUM)" --backtitle "${_TITLE} ($n)" --inputbox "Windows:" $_BOX_H $_BOX_W "${_WIN_LOADED[$n]}" 3>&1 1>&2 2>&3)
         exitstatus=$?
         if [ $exitstatus = 0 ]; then
-            until [ ! -z ${_LIN[$n]} ]; do
-                _LIN[$n]=$(whiptail --title "Paths configuration ($n)" --backtitle "WIN($n)=${_WIN[$n]}" --inputbox "Windows:${_WIN[$n]} \n Linux:" $_BOX_H $_BOX_W "" 3>&1 1>&2 2>&3)
+                _LIN[$n]=$(whiptail --title "${_TITLE} ($n di $_PATH_NUM)" --backtitle "WIN($n)=${_WIN[$n]}" --inputbox "Windows:${_WIN[$n]} \nLinux:" $_BOX_H $_BOX_W "${_LIN_LOADED[$n]}" 3>&1 1>&2 2>&3)
                 exitstatus=$?
                 if [ $exitstatus != 0 ]; then
-                    exit
+                    continue
                 fi
-            done
         else
-            return exit
+            return return
         fi
     done
 }
@@ -27,9 +34,10 @@ function paths_configuration () {
 # Dimensione dialog
 _BOX_H=10
 _BOX_W=80
+_TITLE="Configurazione dei path Windows-Linux"
 
 # Richiede numero di coppie (WIN/LIN) di path da gestire
-_PATH_NUM=$(whiptail --title "Paths configuration" --inputbox "Nr. di path da gestire:" $_BOX_H $_BOX_W 0 3>&1 1>&2 2>&3)
+_PATH_NUM=$(whiptail --title "${_TITLE}" --inputbox "Nr. di path da gestire:" $_BOX_H $_BOX_W "" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ] && [ $_PATH_NUM -gt 0 ]; then
     paths_configuration
@@ -41,6 +49,6 @@ if [ $exitstatus = 0 ] && [ $_PATH_NUM -gt 0 ]; then
     done
     
 else
-    exit
+    return
 fi
 
