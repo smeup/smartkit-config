fbcd5e35e16988e4e472ebcb46ce1aa8e2f5169c
Marco Benetti <marco.benetti@smeup.com>
2019-10-01 12:24:46 +0200

    Full screen windows

--- pathsConf.sh
+++ pathsConf.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Dimensione dialog
-_BOX_H=10
-_BOX_W=80
 
 # Nr. max di path configurabili
 _MAX_NUM_PATHS=9
@@ -35,10 +32,10 @@ function paths_configuration () {
         _WIN[$n]=""
         _LIN[$n]=""
         # Remapping windows/linux path WINnn-LINnn
-        _WIN[$n]=$(whiptail --title "${_TITLE} ($((n+1)) di $_PATH_NUM)" --backtitle "${_TITLE} $((n+1))" --inputbox "Windows:" $_BOX_H $_BOX_W "${_WIN_LOADED[$n]}" 3>&1 1>&2 2>&3)
+        _WIN[$n]=$(whiptail --title "${_TITLE} ($((n+1)) di $_PATH_NUM)" --backtitle "${_TITLE} $((n+1))" --inputbox "Windows:" $(stty size) "${_WIN_LOADED[$n]}" 3>&1 1>&2 2>&3)
         exitstatus=$?
         if [ $exitstatus = 0 ]; then
-                _LIN[$n]=$(whiptail --title "${_TITLE} ($((n+1)) di $_PATH_NUM)" --backtitle "WIN($n)=${_WIN[$n]}" --inputbox "Windows:${_WIN[$n]} \nLinux:" $_BOX_H $_BOX_W "${_LIN_LOADED[$n]}" 3>&1 1>&2 2>&3)
+                _LIN[$n]=$(whiptail --title "${_TITLE} ($((n+1)) di $_PATH_NUM)" --backtitle "WIN($n)=${_WIN[$n]}" --inputbox "Windows:${_WIN[$n]} \nLinux:" $(stty size) "${_LIN_LOADED[$n]}" 3>&1 1>&2 2>&3)
                 exitstatus=$?
                 if [ $exitstatus != 0 ]; then
                     continue
@@ -65,23 +62,23 @@ function save_paths () {
 
 # MAIN
 if [ ! -f "$_FILE_CONFIG" ]; then
-    whiptail --title "Attenzione" --msgbox "File configurazione non trovato, esequire prima 'Configurazioni di base'" $_BOX_H $_BOX_W
+    whiptail --title "Attenzione" --msgbox "File configurazione non trovato, esequire prima 'Configurazioni di base'" $(stty size)
     return
 fi
 
 # Chiede all'utente numero di coppie (WIN/LIN) di path da gestire (previste max 9 coppie)
 while true
 do
-    _PATH_NUM=$(whiptail --title "${_TITLE}" --inputbox "Nr. di path da gestire (max ${_MAX_NUM_PATHS}):" $_BOX_H $_BOX_W "" 3>&1 1>&2 2>&3)
+    _PATH_NUM=$(whiptail --title "${_TITLE}" --inputbox "Nr. di path da gestire (max ${_MAX_NUM_PATHS}):" $(stty size) "" 3>&1 1>&2 2>&3)
     exitstatus=$?
     if [ $exitstatus = 0 ] && [ $_PATH_NUM -gt $_MAX_NUM_PATHS ]; then
-        whiptail --title "Attenzione" --msgbox "Previste al massimo ${_MAX_NUM_PATHS} coppie (WIN/LIN) di path di configurazione" $_BOX_H $_BOX_W
+        whiptail --title "Attenzione" --msgbox "Previste al massimo ${_MAX_NUM_PATHS} coppie (WIN/LIN) di path di configurazione" $(stty size)
         continue
     fi
     if [ $exitstatus = 0 ] && [ $_PATH_NUM -gt 0 ]; then
         paths_configuration
         if [ $n -gt 0 ]; then
-            if (whiptail --title "${_TITLE}" --yesno "Salvare i ${n} paths configurati?" $_BOX_H $_BOX_W); then
+            if (whiptail --title "${_TITLE}" --yesno "Salvare i ${n} paths configurati?" $(stty size)); then
                 save_paths
             fi
             return
